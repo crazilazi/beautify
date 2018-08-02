6a6a505ad8e05db182458ad5257fc51b2f884ae8
RajeevRanjanOneadvanced <rajeev.ranjan@oneadvanced.com>
2018-08-02 15:31:53 +0530

    sidenavbar work in progress

--- app/content.js
+++ app/content.js
@@ -8,5 +8,34 @@ document.addEventListener("mousedown", function (event) {
 // tslint:disable-next-line:typedef
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     // clickedElement.remove();
-    alert($(clickedElement).text().trim());
+    // alert($(clickedElement).text().trim());
+    console.log(clickedElement);
+    console.log($(clickedElement).parent());
+    openNav();
 });
+function injectSideNavBar() {
+    // tslint:disable-next-line:typedef
+    $.get(chrome.extension.getURL("sideNavBar.html"), function (data) {
+        // $(data).appendTo("body");
+        // or if you're using jQuery 1.8+:
+        $($.parseHTML(data)).appendTo("body");
+        $("#closemybeautifynavbar").bind("click", function () {
+            closeNav();
+        });
+    });
+}
+// tslint:disable-next-line:typedef
+function openNav() {
+    var open = document.getElementById("mySidenav");
+    if (open) {
+        open.style.width = "250px";
+    }
+}
+// tslint:disable-next-line:typedef
+function closeNav() {
+    var close = document.getElementById("mySidenav");
+    if (close) {
+        close.style.width = "0px";
+    }
+}
+injectSideNavBar();
--- content.ts
+++ content.ts
@@ -2,13 +2,44 @@
 let clickedElement: any = null;
 
 // tslint:disable-next-line:typedef
-document.addEventListener("mousedown", function(event){
+document.addEventListener("mousedown", function (event) {
   clickedElement = event.target;
 }, true);
 
 // tslint:disable-next-line:typedef
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    // clickedElement.remove();
-    alert($(clickedElement).text().trim());
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // clickedElement.remove();
+  // alert($(clickedElement).text().trim());
+  console.log(clickedElement);
+  console.log($(clickedElement).parent());
+  openNav();
+});
+
+function injectSideNavBar(): void {
+  // tslint:disable-next-line:typedef
+  $.get(chrome.extension.getURL("sideNavBar.html"), function (data) {
+    // $(data).appendTo("body");
+    // or if you're using jQuery 1.8+:
+    $($.parseHTML(data)).appendTo("body");
+    $("#closemybeautifynavbar").bind("click", function () {
+      closeNav();
+    });
+  });
+}
+
+// tslint:disable-next-line:typedef
+function openNav() {
+  let open: any = document.getElementById("mySidenav");
+  if (open) {
+    open.style.width = "250px";
   }
-);
+}
+
+// tslint:disable-next-line:typedef
+function closeNav() {
+  let close: any = document.getElementById("mySidenav");
+  if (close) {
+    close.style.width = "0px";
+  }
+}
+injectSideNavBar();
